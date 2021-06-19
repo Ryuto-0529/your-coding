5dd14482a0f61d85917338eee593dadea65ca8bf
Ryuto-0529 <abababrt29@gmail.com>
2021-06-19 09:39:44 +0900

    third-commit

--- js/main.js
+++ js/main.js
@@ -95,9 +95,21 @@ $('#form').submit(function (event) {
   });
   event.preventDefault();
 });
-
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
 
 AOS.init();
+
+
+
 
 
 
